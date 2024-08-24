1e5a815280e00cfc531d2706ff45db6345a281d3
Mike Lynch <nycynik@gmail.com>
2024-08-24 14:52:38 -0700

    updated directory structure

--- anythingLLM.sh
+++
@@ -1,9 +0,0 @@
-export STORAGE_LOCATION=$AIHOME/Documents 
-mkdir -p $STORAGE_LOCATION
-touch "$STORAGE_LOCATION/.env" 
-
-docker run -d -p 3001:3001 --cap-add SYS_ADMIN \
-   --name anythingllm \
-   -v ${STORAGE_LOCATION}:/app/server/storage \
-   -v ${STORAGE_LOCATION}/.env:/app/server/.env \
-   -e STORAGE_DIR="/app/server/storage" mintplexlabs/anythingllm
